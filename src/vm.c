0fd6e5a8d8842992cc0d10bd279db4a7baa9086d
kwadwo amankwa <citisolo@gmail.com>
2016-06-15 11:52:28 +0100

    added exec_frame function and instruction interface

--- src/vm.c
+++ src/vm.c
@@ -9,9 +9,15 @@
 #define add_sym(symtab, id, val) add_symbol(symtab, int_to_id(id), val)
 #define lookup_sym(symtab, id) lookup_symbol(symtab, int_to_id(id))
 
-void init_constpool(){
+void init(){
 	constant_pool = (Symtab*) malloc(sizeof(Symtab));
 	constant_pool->head = NULL;
+	global_table = (Symtab*) malloc(sizeof(Symtab));
+	global_table->head = NULL;
+	runstack = (RuntimeStack*) malloc(sizeof(RuntimeStack));
+	runstack->top = NULL;
+	opstack = (OperandStack*) malloc(sizeof(OperandStack));
+	opstack->top = NULL;
 }
 
 char* int_to_id(int id ){
@@ -66,6 +72,13 @@ void* allocate_obj(Type type){
 		    res = obj;
 		    break;
 		}
+		case OPERAND:{
+		    Operand* obj = (Operand*)malloc(sizeof(Operand));
+		    obj->type = OPERAND;
+		    obj->next = NULL;
+		    res = obj;
+		    break;
+		}
 		default:
 		    printf("Cannot allocate for unknown type %d\n", type);
             exit(-1);
@@ -73,7 +86,6 @@ void* allocate_obj(Type type){
 	}
 	return res;
 }
-
 
 void add_symbol(Symtab* symtab, char* id, Value_t* val ){
 	/*create the new symbol */
@@ -98,29 +110,91 @@ void add_symbol(Symtab* symtab, char* id, Value_t* val ){
 	}
 	
 }
-	
-	
-Value_t* lookup_symbol(Symtab* symtab, char* id){
+		
+Symbol* lookup_symbol(Symtab* symtab, char* id){
 	Symbol* node = symtab->head;
     if (node == NULL){
 		return NULL;	
 	}else{
 		while(node != NULL){
 			if(strcmp(node->id, id) == 0){
-				return node->object;
+				return node;
 			} 
 			node = node->next;
 		}
 	}
 	return NULL;
 }
-
 	
+void rs_push (RuntimeStack* stack, Record* frame){
+	if(stack->top == NULL){
+		stack->top = frame;
+	}else{
+        frame->parent = stack->top;
+        stack->top = frame;	
+	}
+}
+
+Record* rs_pop (RuntimeStack* stack){
+	Record* rec = stack->top;
+	
+	if (stack->top == NULL){
+		return NULL;
+	}else{
+	   stack->top = rec->parent;
+    }
+	return rec;
+}
+
+void os_push (OperandStack* stack, Value_t* operand){
+	Operand* op = (Operand*) malloc(sizeof(Operand));
+	op->val = operand;
+	op->next = NULL;
+	
+	if (stack->top == NULL){
+		stack->top = op;
+	}else{
+		op->next = stack->top;
+		stack->top = op;
+	}
+		
+}
+
+Value_t* os_pop (OperandStack* stack){
+	
+    Operand* op = stack->top;
+    if ( op == NULL){
+		return NULL;
+	}else{
+	    stack->top = op->next;	
+	}
+	 
+	return op->val;
+	
+}
+
+Value_t* os_peek(OperandStack* stack , int i){
+	Operand* op = stack->top;
+	if(i == 0){
+		return op->val;
+	}
+	while(op->next != NULL && i != 0){
+		op = op->next;
+		i--;
+	}
+    return op->val;
+}
+
+Record* new_frame(Record* parent){
+	Record* frame = (Record*) malloc(sizeof(Record));
+	frame->parent = parent;
+	return frame;
+}
+
 void add_to_constpool (int id, Value* v) {
   switch(v->tag){
   case INT_VAL:{
     IntValue* v2 = (IntValue*)v;
-    
     IntObj* num = (IntObj*)allocate_obj(INT_TYPE);
     num->type = INT_TYPE;
     num->val = v2->value;
@@ -146,7 +220,7 @@ void add_to_constpool (int id, Value* v) {
     MethodValue* v2 = (MethodValue*)v;
     
     MethodObj* val = (MethodObj*)allocate_obj(METHOD);
-    StringObj* string = (StringObj*)lookup_sym(constant_pool, v2->name );
+    StringObj* string = (StringObj*)lookup_sym(constant_pool, v2->name )->object;
     if( string != NULL){
 		val->index = string;
 	}else{
@@ -162,7 +236,7 @@ void add_to_constpool (int id, Value* v) {
   case SLOT_VAL:{
     SlotValue* v2 = (SlotValue*)v;
     SlotObj* val = (SlotObj*)allocate_obj(SLOT);
-    StringObj* string = (StringObj*)lookup_sym(constant_pool, v2->name);
+    StringObj* string = (StringObj*)lookup_sym(constant_pool, v2->name)->object;
     if( string != NULL){
 		val->index = string;
 	}else{
@@ -185,21 +259,163 @@ void add_to_constpool (int id, Value* v) {
   }
 }
 
+void add_to_globtable (int id){
+	Symbol* sym = lookup_sym(constant_pool, id);
+	Value_t* v = sym->object;
+    switch(v->type){
+		case SLOT:{
+			SlotObj* v2 = (SlotObj*)v;
+			add_symbol(global_table, v2->index->val, v2);
+			break;
+		}
+		case METHOD:{
+			MethodObj* v2 = (MethodObj*)v;
+			add_symbol(global_table, v2->index->val, v2);
+			break;
+		}
+		default:
+          printf("Value with unknown tag: %d\n", v->type);
+          exit(-1);
+  }	
+	
+}
+
+void execute_frame(int fn_index, Record* parent){
+	Symbol* fs = lookup_sym(constant_pool, fn_index);
+	if(fs == NULL){
+		printf("Method: %d not defined", fn_index);
+		exit(1);
+	}
+	MethodObj* fn = (MethodObj*)fs->object;
+	if(fn->type != METHOD){
+	    printf("cannot execute %d it is not a method", fn_index);
+		exit(1);
+	}
+	
+	Record* frame = new_frame(parent);
+	/* add args to frame */
+	for (int i = 0; i < fn->nargs; i++) {
+		frame->args[i] =(SlotObj*)os_pop(opstack);
+	}
+	/* add locals to frame */
+    for (int i = 0; i < fn->nlocals; i++) {
+		frame->locals[i] =(SlotObj*)os_pop(opstack);
+	}
+	
+	for(int i=0; i<fn->body->size; i++){
+		ByteIns* ins = vector_get(fn->body, i);
+		switch(ins->tag){
+		  case LABEL_OP:{
+			LabelIns* i = (LabelIns*)ins;
+			printf("label #%d", i->name);
+			break;
+		  }
+		  case LIT_OP:{
+			LitIns* i = (LitIns*)ins;
+			Symbol* s = lookup_sym(constant_pool, i->idx);
+			if (s == NULL){
+				printf(" symbol '%d' not defined ", i->idx);
+				exit(1);
+			}
+			os_push(opstack, s->object);
+			break;
+		  }
+		  case PRINTF_OP:{
+			PrintfIns* i = (PrintfIns*)ins;
+			printf("   printf #%d %d", i->format, i->arity);
+			break;
+		  }
+		  case ARRAY_OP:{
+			printf("   array");
+			break;
+		  }
+		  case OBJECT_OP:{
+			ObjectIns* i = (ObjectIns*)ins;
+			printf("   object #%d", i->class);
+			break;
+		  }
+		  case SLOT_OP:{
+			SlotIns* i = (SlotIns*)ins;
+			printf("   slot #%d", i->name);
+			break;
+		  }
+		  case SET_SLOT_OP:{
+			SetSlotIns* i = (SetSlotIns*)ins;
+			printf("   set-slot #%d", i->name);
+			break;
+		  }
+		  case CALL_SLOT_OP:{
+			CallSlotIns* i = (CallSlotIns*)ins;
+			printf("   call-slot #%d %d", i->name, i->arity);
+			break;
+		  }
+		  case CALL_OP:{
+			CallIns* i = (CallIns*)ins;
+			printf("   call #%d %d", i->name, i->arity);
+			break;
+		  }
+		  case SET_LOCAL_OP:{
+			SetLocalIns* i = (SetLocalIns*)ins;
+			printf("   set local %d", i->idx);
+			break;
+		  }
+		  case GET_LOCAL_OP:{
+			GetLocalIns* i = (GetLocalIns*)ins;
+			printf("   get local %d", i->idx);
+			break;
+		  }
+		  case SET_GLOBAL_OP:{
+			SetGlobalIns* i = (SetGlobalIns*)ins;
+			printf("   set global #%d", i->name);
+			break;
+		  }
+		  case GET_GLOBAL_OP:{
+			GetGlobalIns* i = (GetGlobalIns*)ins;
+			printf("   get global #%d", i->name);
+			break;
+		  }
+		  case BRANCH_OP:{
+			BranchIns* i = (BranchIns*)ins;
+			printf("   branch #%d", i->name);
+			break;
+		  }
+		  case GOTO_OP:{
+			GotoIns* i = (GotoIns*)ins;
+			printf("   goto #%d", i->name);
+			break;
+		  }
+		  case RETURN_OP:{
+			printf("   return");
+			break;
+		  }
+		  case DROP_OP:{
+			printf("   drop");
+			break;
+		  }
+		  default:{
+			printf("Unknown instruction with tag: %u\n", ins->tag);
+			exit(-1);
+		  }
+		 }
+	}
+}
 
 void run_prog (Program* p) {
-  init_constpool();
+  init();
   /* Add constants to symnbol table */
   Vector* vs = p->values;
   for(int i=0; i<vs->size; i++){
     add_to_constpool(i, vector_get(vs, i));
   }
   
-  /* execute code */
-  for(int i=0; i<p->slots->size; i++)
+  /* add global symbols */
+  for(int i=0; i<p->slots->size; i++){
     printf("\n   #%d", (int)vector_get(p->slots, i));
+    add_to_globtable((int)vector_get(p->slots,i));
+  }
+  execute_frame(p->entry, NULL);
   printf("\nEntry : #%d", p->entry);
 }
-
 
 void interpret_bc (Program* p) {
   printf("Interpreting Bytecode Program:\n");
--- src/vm.h
+++ src/vm.h
@@ -9,44 +9,17 @@ typedef enum {
     SLOT,
 	METHOD,
 	CLASS,
-	SYMBOL
+	SYMBOL,
+	OPERAND
 }Type;
 
+typedef struct Symbol Symbol;
+typedef struct Record Record;
+typedef struct Operand Operand;
 
 typedef struct {
 	int type;
 }Value_t;
-
-
-
-typedef struct {
-	int type;
-	char* id;
-	Value_t* object;
-	struct Symbol* next;
-}Symbol;
-
-typedef struct {
-	Symbol* head;
-}Symtab;
-
-Symtab* constant_pool;
-
-void init_constpool();
-void add_symbol(Symtab* symtab, char* id, Value_t* val );
-Value_t* lookup_symbol(Symtab* symtab, char* id);
-
-
-typedef struct {
-	struct Record* next;
-}Record;
-
-typedef struct {
-	Record* top;
-}Stack;
-
-void push (Stack* stack, Record* slot);
-Record* pop (Stack* stack);
 
 /* Objects */
 typedef struct {
@@ -84,10 +57,80 @@ typedef struct {
 }ClassObj;
 
 
+struct Symbol{
+	int type;
+	char* id;
+	Value_t* object;
+	Symbol* next;
+};
+
+typedef struct {
+	Symbol* head;
+}Symtab;
+
+Symtab* constant_pool;
+Symtab* global_table;
+
+void init_constpool();
+void add_symbol(Symtab* symtab, char* id, Value_t* val );
+Symbol* lookup_symbol(Symtab* symtab, char* id);
+
+
+struct Record {
+	SlotObj** args;
+	SlotObj** locals;
+	Record* parent;
+};
+
+typedef struct {
+	Record* top;
+}RuntimeStack;
+
+RuntimeStack* runstack;
+
+void rs_push (RuntimeStack* stack, Record* slot);
+Record* rs_pop (RuntimeStack* stack);
+Record* new_frame( Record* parent); 
+
+struct Operand{
+	Value_t* val;
+	struct Operand* next;
+};
+
+typedef struct {
+	Operand* top;
+}OperandStack;
+
+OperandStack* opstack;
+
+void os_push (OperandStack* stack, Value_t* slot);
+Value_t* os_pop (OperandStack* stack);
+Value_t* os_peek(OperandStack* stack, int i);
+
+typedef struct {
+	ByteIns* ins;
+}ProgramCounter;
 
 
 /* Instructions */
-void lit(Symtab* constant_pool, Value_t* val);
+void LIT(int i);
+void ARRAY();
+void PLIT(char* format, int n);
+void SETLOCAL(int i);
+void GETLOCAL(int i);
+void SETGLOBAL(int i);
+void GETGLOBAL(int i);
+void DROP();
+
+void OBJECT(int i);
+void GETSLOT(int i);
+void CALLSLOT(int i, int n);
+
+void LABEL(int i);
+void BRANCH(int i);
+void GOTO(int i);
+void RETURN();
+void CALL(int i, int n);
 
 
 void* allocate_obj(Type type);
